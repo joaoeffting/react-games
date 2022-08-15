ff0d589718c099194eebcfc0bd3be6fcb0105da8
Joao Effting <joaopaulo.effting@kindredgroup.com>
2022-08-15 12:42:24 +0200

    Adding jest to the project and writing tests for getDeckById

---
+++ src/utils/__tests__/getDeckById.test.ts
@@ -0,0 +1,22 @@
+import getDeckById from "../getDeckById";
+import { POKEMON_DECK } from "../../components/Memory/decks/pokemon";
+import { DIGIMON_DECK } from "../../components/Memory/decks/digimon";
+
+describe("getDeckById", () => {
+  it("Should return the POKEMON deck", () => {
+    const deck = getDeckById("pokemon");
+    expect(deck).toHaveLength(6);
+    expect(deck).toMatchObject(POKEMON_DECK);
+  });
+
+  it("Should return the DIGIMON deck", () => {
+    const deck = getDeckById("digimon");
+    expect(deck).toHaveLength(6);
+    expect(deck).toMatchObject(DIGIMON_DECK);
+  });
+
+  it("Should return the UNDEFINED if id doesn't exists", () => {
+    const deck = getDeckById("sakura");
+    expect(deck).toBe(undefined);
+  });
+});
