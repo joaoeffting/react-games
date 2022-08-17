fad425203825270b830b768c73bbf630a62e4e92
Joao Effting <joaopaulo.effting@kindredgroup.com>
2022-08-17 13:32:30 +0200

    testing 2

--- pages/api/getDeckById/index.ts
+++ pages/api/getDeckById/index.ts
@@ -10,6 +10,4 @@ export default async function getAllGames(
   const deck = await db.collection("decks").findOne({ deck_name: "pokemon" });
   console.log("ERRO 2", deck);
   if (deck) res.status(200).json(deck.deck);
-
-  res.status(404).json({ message: "Deck not found" });
 }
