f1c51744fb9e3401dac70dcdbca6bbbef4048813
Joao Effting <joaopaulo.effting@kindredgroup.com>
2022-08-17 13:21:02 +0200

    Adding console.log

--- pages/api/getDeckById/[id].ts
+++ pages/api/getDeckById/[id].ts
@@ -7,8 +7,9 @@ export default async function getAllGames(
 ) {
   let { db } = await connectToDatabase();
   // const { id } = req.query;
+  console.log("ERRO 1");
   const deck = await db.collection("decks").findOne({ deck_name: "pokemon" });
-
+  console.log("ERRO 2", deck);
   if (deck) res.status(200).json(deck.deck);
 
   res.status(404).json({ message: "Deck not found" });
