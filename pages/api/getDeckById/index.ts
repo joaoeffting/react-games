5fb3431acd0ec40df95adb2a9332cf267a56d15f
Joao Effting <joaopaulo.effting@kindredgroup.com>
2022-08-17 13:31:05 +0200

    Testingn another route

---
+++ pages/api/getDeckById/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../lib/mongodb";
+
+export default async function getAllGames(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let { db } = await connectToDatabase();
+  console.log("ERRO 1");
+  const deck = await db.collection("decks").findOne({ deck_name: "pokemon" });
+  console.log("ERRO 2", deck);
+  if (deck) res.status(200).json(deck.deck);
+
+  res.status(404).json({ message: "Deck not found" });
+}
